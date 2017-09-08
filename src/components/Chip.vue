b5cc41aa1f6ba7163af887338dd65ffe6f2e3f99
andoshin11 <shinglish11@gmail.com>
2017-09-09 00:27:50 +0900

    Init board

---
+++ src/components/Chip.vue
@@ -0,0 +1,34 @@
+<template>
+      <div class="chip blue" style="position:absolute;" :style="{ top: top, left: left, backgroundColor: chip.color }"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    chip: {
+      type: Object,
+      required: true
+    }
+  },
+  name: 'chip',
+  computed: {
+    top () {
+      return (((this.chip.row - 1) * 200) + -30) + 'px'
+    },
+    left () {
+      return (((this.chip.column - 1) * 200) + -30) + 'px'
+    }
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+
+</style>
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,38 +1,180 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div class="board">
+      <table class="grid">
+        <tr>
+          <td class="grid__cell"></td>
+          <td class="grid__cell"></td>
+          <td class="grid__cell"></td>
+          <td class="grid__cell"></td>
+        </tr>
+        <tr>
+          <td class="grid__cell"></td>
+          <td class="grid__cell"></td>
+          <td class="grid__cell"></td>
+          <td class="grid__cell"></td>
+        </tr>
+        <tr>
+          <td class="grid__cell"></td>
+          <td class="grid__cell"></td>
+          <td class="grid__cell"></td>
+          <td class="grid__cell"></td>
+        </tr>
+        <tr>
+          <td class="grid__cell"></td>
+          <td class="grid__cell"></td>
+          <td class="grid__cell"></td>
+          <td class="grid__cell"></td>
+        </tr>
+      </table>
+      <chip :chip="chip" v-for="chip in chips"/>
+    </div>
   </div>
 </template>
 
 <script>
+import chip from './Chip.vue'
 export default {
-  name: 'hello',
+  name: 'game',
+  components: {
+    chip
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      chips: [
+        {
+          color: 'blue',
+          row: 1,
+          column: 1
+        },
+        {
+          color: 'red',
+          row: 1,
+          column: 2
+        },
+        {
+          color: 'green',
+          row: 1,
+          column: 3
+        },
+        {
+          color: 'yellow',
+          row: 1,
+          column: 4
+        },
+        {
+          color: 'transparent',
+          row: 1,
+          column: 5
+        },
+        {
+          color: 'yellow',
+          row: 2,
+          column: 1
+        },
+        {
+          color: 'transparent',
+          row: 2,
+          column: 2
+        },
+        {
+          color: 'blue',
+          row: 2,
+          column: 3
+        },
+        {
+          color: 'red',
+          row: 2,
+          column: 4
+        },
+        {
+          color: 'green',
+          row: 2,
+          column: 5
+        },
+        {
+          color: 'green',
+          row: 3,
+          column: 1
+        },
+        {
+          color: 'yellow',
+          row: 3,
+          column: 2
+        },
+        {
+          color: 'transparent',
+          row: 3,
+          column: 3
+        },
+        {
+          color: 'blue',
+          row: 3,
+          column: 4
+        },
+        {
+          color: 'red',
+          row: 3,
+          column: 5
+        },
+        {
+          color: 'transparent',
+          row: 4,
+          column: 1
+        },
+        {
+          color: 'blue',
+          row: 4,
+          column: 2
+        },
+        {
+          color: 'red',
+          row: 4,
+          column: 3
+        },
+        {
+          color: 'green',
+          row: 4,
+          column: 4
+        },
+        {
+          color: 'yellow',
+          row: 4,
+          column: 5
+        },
+        {
+          color: 'red',
+          row: 5,
+          column: 1
+        },
+        {
+          color: 'green',
+          row: 5,
+          column: 2
+        },
+        {
+          color: 'yellow',
+          row: 5,
+          column: 3
+        },
+        {
+          color: 'transparent',
+          row: 5,
+          column: 4
+        },
+        {
+          color: 'blue',
+          row: 5,
+          column: 5
+        }
+      ]
     }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
 h1, h2 {
   font-weight: normal;
 }
@@ -50,4 +192,32 @@ li {
 a {
   color: #42b983;
 }
+//
+// .blue {
+//   background-color: blue;
+// }
+
+.board {
+  margin: auto;
+  width: 800px;
+  height: 800px;
+  position: relative;
+}
+
+.chip {
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+}
+
+.grid {
+  width: 100%;
+  height: 100%;
+  border-collapse: collapse;
+  margin: auto;
+  &__cell {
+    border: solid 1px grey;
+  }
+}
+
 </style>
