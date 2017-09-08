eeefbaf4d44208c4fdcc88304ebb9c9e56080fca
andoshin11 <shinglish11@gmail.com>
2017-09-08 23:27:58 +0900

    Setup Game path

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Game from '@/components/Game'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/game',
+      name: 'Game',
+      component: Game
     }
   ]
 })
